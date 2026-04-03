fe4228af5f6afaa0e4d1e5035213b1c9d88ff12d
Naitik-Upadhayay-27 <upadhayaynaitik2712@gmail.com>
2026-04-03 02:11:45 -0700

    gf

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route, Navigate, useEffect } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import LandingPage from '@/pages/LandingPage'
 import LoginPage from '@/pages/LoginPage'
 import SignupPage from '@/pages/SignupPage'
